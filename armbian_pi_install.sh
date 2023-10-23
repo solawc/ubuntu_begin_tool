b09c7bdad96ee7ba55e1cecf51e37c153dbfe628
solawc <1225093964@qq.com>
2023-10-23 15:55:41 +0800

    update

--- armbian_pi_install.sh
+++ armbian_pi_install.sh
@@ -22,6 +22,7 @@ else
 fi
 
 #检查是否安装git
+
 echo "检查是否已经安装了git....."
 if ! dpkg -s git >/dev/null 2>&1; then
         sudo apt install -y git
@@ -30,24 +31,11 @@ else
         echo "git已经安装了！"
 fi
 
-# cd ~/
-# git clone https://github.com/th33xitus/kiauh.git
+cd ~/
+git clone https://github.com/th33xitus/kiauh.git
 
-# cd ~/kiauh
-# ./kiauh.sh
-
-# 检查是否已安装VS Code
- if ! dpkg -s code >/dev/null 2>&1; then
-   # 下载并安装VS Code
-   echo "下载并安装vscode"
-   wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-   sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-   sudo apt-get update
-   sudo apt-get install -y code
- else
-   echo "VS Code已安装"
- fi
-
+cd ~/kiauh
+./kiauh.sh
 
 
 
